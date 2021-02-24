4d8236c6dbffbfa1aa0642e319558bd805326e9e
eztaskomaz <eztaskomaz@gmail.com>
2021-02-24 23:58:20 +0300

    adding components to show corona info

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import './App.css';
+import React from 'react';
+import styles from './App.module.css';
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+import { fetchData } from "./api";
+import coronaImage from './images/image.png'
 
-const App = () => {
+class App extends React.Component {
     //const api_key = process.env.REACT_APP_KEY;
 
-    return (
-        <div className="App">
-            <h1>Corona Tracker App</h1>
-        </div>
-    );
+    state = {
+        data: {},
+        country: ''
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({data: fetchedData});
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({data: fetchedData, country: country});
+    }
+
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+                <Cards data={data}/>
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
+            </div>
+        );
+    }
 }
 
 export default App;
